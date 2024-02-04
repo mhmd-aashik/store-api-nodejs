0fa2ed03a9b22e1a8a1ecd1faff3149ab122230f
aashik <aashikdevelop@gmail.com>
2024-02-05 04:43:33 +0530

    Select Option

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -26,9 +26,13 @@ const getAllProducts = async (req, res) => {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
-    result = result.sort;
+    result = result.sort("createdAt");
   }
 
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
   const products = await result;
   res.status(200).json({ length: products.length, data: products });
 };
