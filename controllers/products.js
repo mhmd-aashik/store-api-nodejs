c035001850c6d9387c36f02ecf100ed9418e891e
aashik <aashikdevelop@gmail.com>
2024-02-05 04:14:17 +0530

    Name

--- controllers/products.js
+++ controllers/products.js
@@ -6,15 +6,19 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  if(company) {
+  if (company) {
     queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
 
   const products = await Product.find(queryObject);
