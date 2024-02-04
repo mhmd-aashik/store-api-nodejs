7976151d2f77f8c4699a6f9195a4d903d2a43800
aashik <aashikdevelop@gmail.com>
2024-02-04 15:27:13 +0530

    Basic Express App

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('04 Store API')
+const express = require("express");
+const app = express();
+
+const notFoundMiddleWare = require("./middleware/not-found");
+const errorMiddleWare = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send('<h1>Home Page</h1> <a href="/api/v1/products">products page</a>');
+});
+
+app.use(notFoundMiddleWare);
+app.use(errorMiddleWare);
+
+const port = 3000;
+
+const start = () => {
+  app.listen(port, console.log(`🟢 port is listening on ${port}`));
+  try {
+  } catch (error) {
+    console.log(`🔴 , error`);
+  }
+};
+
+start();
